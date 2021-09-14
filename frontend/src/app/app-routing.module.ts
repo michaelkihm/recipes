5c02888babd8988c1d16c98e294315a1bfe10c83
Michael <michael.kihm@web.de>
2021-09-14 18:48:15 +0200

    Add general component structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/recipes', pathMatch: 'full' },
+	{ path: 'recipes', component: RecipesComponent, children: [
+		{ path: ':id', component: RecipeDetailComponent }
+	] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeCardComponent } from './recipes/recipe-card/recipe-card.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    HeaderComponent,
+    RecipeCardComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
