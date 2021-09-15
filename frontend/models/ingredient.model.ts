34703bf438203c9ae611d2111be6d7c13b0ae8dd
Michael <michael.kihm@web.de>
2021-09-15 20:49:31 +0200

    add Unit type to Ingredient

--- frontend/models/ingredient.model.ts
+++ frontend/models/ingredient.model.ts
@@ -1,6 +1,8 @@
+type Unit = 'ml' | 'kg' | 'g' | 'pieces' | 'teaspoon' | 'tablespoon' | 'hands';
 export class Ingredient {
     constructor(
         public name: string,
         public amount: number,
+        public unit: Unit
     ) {}
 }
