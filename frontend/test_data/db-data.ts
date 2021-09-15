3af5cfa29edf20f843fe1483b840962cf1a99261
Michael <michael.kihm@web.de>
2021-09-15 20:56:41 +0200

    Add some dummy recipes data

---
+++ frontend/test_data/db-data.ts
@@ -0,0 +1,53 @@
+/* eslint-disable max-len */
+import { Recipe } from 'models/recipes.model';
+
+export const RECIPES: Recipe[] = [
+    {
+        name: 'Spaghetti aglio',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        createdBy: 'user1',
+        id: '0',
+        category: ['italian', 'pasta'],
+        ingredients: [
+            { name: 'tomatoes', amount: 2, unit: 'pieces' },
+            { name: 'garlic', amount: 5, unit: 'pieces' },
+            { name: 'parsley', amount: 1, unit: 'pieces' }
+        ]
+    },
+    {
+        name: 'Burger',
+        description: 'Lorem ipsum dolor sit amet, consectetur. Duis aute irure dolor in reprehenderit in voluptate dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        createdBy: 'user1',
+        id: '1',
+        category: ['bbq'],
+        ingredients: [
+            { name: 'tomatoes', amount: 2, unit: 'pieces' },
+            { name: 'bread', amount: 2, unit: 'slices' },
+            { name: 'salat', amount: 3, unit: 'pieces' }
+        ]
+    },
+    {
+        name: 'Falafel',
+        description: 'Vel pretium lectus quam id leo in vitae. Fames ac turpis egestas sed tempus urna et. Dui id ornare arcu odio ut sem nulla pharetra. Pretium fusce id velit ut tortor pretium. Mauris augue neque gravida in fermentum et sollicitudin ac. Sed adipiscing diam donec adipiscing tristique. Viverra adipiscing at in tellus. Fusce id velit ut tortor pretium. In massa tempor nec feugiat nisl pretium. Potenti nullam ac tortor vitae purus.',
+        createdBy: 'user2',
+        id: '2',
+        category: ['vegan', 'arabic'],
+        ingredients: [
+            { name: 'chickpeas', amount: 12, unit: 'g' },
+            { name: 'capsicum', amount: 7 , unit: 'pieces' },
+            { name: 'tahin', amount: 1, unit: 'ml' }
+        ]
+    },
+    {
+        name: 'Recipe',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium quam vulputate dignissim suspendisse in est ante in nibh. Porta nibh venenatis cras sed felis eget velit aliquet. Dolor purus non enim praesent elementum facilisis leo vel. Sit amet mauris commodo quis imperdiet. Sem fringilla ut morbi tincidunt augue. Mattis nunc sed blandit libero. Tortor at auctor urna nunc id. Sapien eget mi proin sed libero enim sed faucibus. Massa massa ultricies mi quis hendrerit dolor magna eget. Posuere ac ut consequat semper. Ut enim blandit volutpat maecenas volutpat blandit aliquam. Aliquet bibendum enim facilisis gravida neque. Volutpat diam ut venenatis tellus in metus vulputate eu. Bibendum neque egestas congue quisque egestas diam in arcu cursus. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Gravida rutrum quisque non tellus. Vulputate sapien nec sagittis aliquam.',
+        createdBy: 'user4',
+        id: '3',
+        category: ['chicken', 'forTheWeekend'],
+        ingredients: [
+            { name: 'meat', amount: 4, unit: 'g' },
+            { name: 'water', amount: 2, unit: 'ml' },
+            { name: 'onion', amount: 4, unit: 'pieces' },
+        ]
+    }
+];
