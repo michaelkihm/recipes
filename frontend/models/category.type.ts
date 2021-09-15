a59dcf244e9ccec1a32bb81ad7d390b3b319692e
Michael <michael.kihm@web.de>
2021-09-15 21:44:59 +0200

    Change type Category

--- frontend/models/category.type.ts
+++ frontend/models/category.type.ts
@@ -1,4 +1,7 @@
-export type Category =
-    'vegan' | 'vegetarian' | 'quick' | 'chicken' | 'pasta' | 'babyFriendly' |
-    'forTheWeekend' | 'salat' | 'italian' | 'spanish' | 'thaiViet' | 'indian' |
-    'healthy' | 'germanMiddleEuropean' | 'bbq' | 'dessert' | 'arabic';
+export const ALL_CATEGORIES = [
+    'vegan' , 'vegetarian' , 'quick' , 'chicken' , 'pasta' , 'babyFriendly' ,
+    'forTheWeekend' , 'salat' , 'italian' , 'spanish' , 'thaiViet' , 'indian' ,
+    'healthy' , 'germanMiddleEuropean' , 'bbq' , 'dessert' , 'arabic'
+] as const;
+
+export type Category = typeof ALL_CATEGORIES[number];
