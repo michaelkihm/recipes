9da0f3688bfaa6bef4fc3a548486887cfc92ced3
Michael <michael.kihm@web.de>
2021-09-23 10:29:32 +0200

    Move recipes controller to separate controller file

---
+++ backend/src/controllers/recipes.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { Recipe } from '../../../models/recipe.model';
+import { RECIPES } from '../../../test_data/db-data';
+
+export type RecipesGetResponse = {
+    message: string,
+    recipes: Recipe[]
+};
+
+export type SingleRecipeResponse = {
+    message: string,
+    recipe?: Recipe
+};
+
+export const getRecipes = (_req: Request, res: Response<RecipesGetResponse>): void => {
+    res.status(200).json({ message: `Have ${RECIPES.length} recipes`, recipes: RECIPES });
+};
+
+export const getRandomRecipes = (req: Request<{amount: string}>, res: Response<RecipesGetResponse>): void => {
+
+    if(+req.params.amount < RECIPES.length){
+        res.status(200).json({
+            message: `Return ${req.params.amount} recipes`,
+            recipes: RECIPES.sort(() => Math.random() - Math.random()).slice(0, +req.params.amount)
+        });
+    } else {
+        res.status(404).json({
+            message: `Number ${req.params.amount} is longer than the list of recipes`,
+            recipes: []
+        });
+    }
+};
+
+
+export const getRecipe = (req: Request<{id: string}>, res: Response<SingleRecipeResponse>): void => {
+
+    const recipe = RECIPES.find(recipe => recipe.id === req.params.id);
+    if(recipe) res.status(200).json({ message: `Found recipe ${req.params.id}`, recipe });
+    else res.status(404).json({ message: `Could not find ${req.params.id}` });
+};
--- backend/src/routes/recipes.routes.ts
+++ backend/src/routes/recipes.routes.ts
@@ -1,41 +1,12 @@
-import { Request, Response, Router } from 'express';
-import { Recipe } from '../../../models/recipe.model';
-import { RECIPES } from '../../../test_data/db-data';
+import { Router } from 'express';
+import { getRandomRecipes, getRecipe, getRecipes } from '../controllers/recipes.controller';
 
 export const recipeRouter = Router();
 
-export type RecipesGetResponse = {
-    message: string,
-    recipes: Recipe[]
-};
 
-export type SingleRecipeResponse = {
-    message: string,
-    recipe?: Recipe
-};
+recipeRouter.get('', getRecipes);
 
-recipeRouter.get('',(req, res: Response<RecipesGetResponse>) => {
-    res.status(200).json({ message: `Have ${RECIPES.length} recipes`, recipes: RECIPES });
-});
+recipeRouter.get('/random/:amount', getRandomRecipes);
 
-recipeRouter.get('/random/:amount', (req: Request<{amount: string}>, res: Response<RecipesGetResponse>) => {
-    if(+req.params.amount < RECIPES.length){
-        res.status(200).json({
-            message: `Return ${req.params.amount} recipes`,
-            recipes: RECIPES.sort(() => Math.random() - Math.random()).slice(0, +req.params.amount)
-        });
-    } else {
-        res.status(404).json({
-            message: `Number ${req.params.amount} is longer than the list of recipes`,
-            recipes: []
-        });
-    }
-});
-
-recipeRouter.get('/:id',(req: Request<{id: string}>, res: Response<SingleRecipeResponse>) => {
-
-    const recipe = RECIPES.find(recipe => recipe.id === req.params.id);
-    if(recipe) res.status(200).json({ message: `Found recipe ${req.params.id}`, recipe });
-    else res.status(404).json({ message: `Could not find ${req.params.id}` });
-});
+recipeRouter.get('/:id',getRecipe);
 
