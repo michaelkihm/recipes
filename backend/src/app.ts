ffd6d60575664c8906c49ba5875fd5eb85b28417
Michael <michael.kihm@web.de>
2021-10-07 11:09:36 +0200

    Use tabs to indent

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,14 +15,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((_req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader(
+    	'Access-Control-Allow-Headers',
+    	'Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, OPTIONS');
+	next();
 });
 
 app.use('/api/recipes',recipeRouter);
