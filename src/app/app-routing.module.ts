366fc0dbd69b9b6f77fd9a40f959e0d9646c75a4
Michael <michael.kihm@web.de>
2021-10-19 14:08:25 +0200

    Added raw version of authorization components - signup-component - login-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolver } from './recipes/recipe-resolver.service';
@@ -10,6 +11,7 @@ const routes: Routes = [
 	{ path: 'recipes', component: RecipesComponent },
 	{ path: 'recipes/:id/edit', component: RecipeEditComponent, resolve: { recipe: RecipeResolver } },
 	{ path: 'recipes/:id', component: RecipeDetailComponent, resolve: { recipe: RecipeResolver } },
+	{ path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { allIcons, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesListComponent } from './recipes/categories-list/categories-list.component';
 import { RecipeCardComponent } from './recipes/recipe-card/recipe-card.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
-
 
 @NgModule({
   declarations: [
@@ -22,13 +23,16 @@ import { RecipesComponent } from './recipes/recipes.component';
     RecipeCardComponent,
     RecipeDetailComponent,
     CategoriesListComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
     ReactiveFormsModule,
     NgxBootstrapIconsModule.pick(allIcons)
   ],
