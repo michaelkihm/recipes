e5efd3934d0987f8f8274d899c8901203372c26f
Michael <michael.kihm@web.de>
2021-10-26 09:36:25 +0200

    Add basic signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResolver } from './recipes/recipe-resolver.service';
@@ -12,6 +13,7 @@ const routes: Routes = [
 	{ path: 'recipes/:id/edit', component: RecipeEditComponent, resolve: { recipe: RecipeResolver } },
 	{ path: 'recipes/:id', component: RecipeDetailComponent, resolve: { recipe: RecipeResolver } },
 	{ path: 'login', component: LoginComponent },
+	{ path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
