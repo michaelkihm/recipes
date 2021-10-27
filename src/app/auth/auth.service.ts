c700cbee5eb66645e8dd38c1ac276394463ba198
Michael <michael.kihm@web.de>
2021-10-27 09:52:25 +0200

    Bugfix Wrong url in login method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -34,41 +34,38 @@ export class AuthService {
     }
 
     login(email: string, password: string): void {
+
         const authData: AuthData = { email: email, password: password };
-        this.http
-          .post<{ token: string; expiresIn: number }>(
-            'http://localhost:3000/api/user/login',
-            authData
-          )
-          .subscribe(response => {
-            const token = response.token;
-            this.token = token;
-            if (token) {
-              const expiresInDuration = response.expiresIn;
-              this.setAuthTimer(expiresInDuration);
-              this.isAuthenticated = true;
-              this.authStatusListener.next(true);
-              const now = new Date();
-              const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
-              console.log(expirationDate);
-              this.saveAuthData(token, expirationDate);
-              this.router.navigate(['/']);
-            }
+        this.http.post<{ token: string; expiresIn: number }>(`${this.baseUrl}/login`,authData)
+          	.subscribe(response => {
+				const token = response.token;
+				this.token = token;
+				if (token) {
+				const expiresInDuration = response.expiresIn;
+				this.setAuthTimer(expiresInDuration);
+				this.isAuthenticated = true;
+				this.authStatusListener.next(true);
+				const now = new Date();
+				const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+				console.log(expirationDate);
+				this.saveAuthData(token, expirationDate);
+				this.router.navigate(['/']);
+				}
           });
       }
     
     autoAuthUser(): void {
+
         const authInformation = this.getAuthData();
-        if (!authInformation) {
-          return;
-        }
+        if (!authInformation) return;
+        
         const now = new Date();
         const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
         if (expiresIn > 0) {
-          this.token = authInformation.token;
-          this.isAuthenticated = true;
-          this.setAuthTimer(expiresIn / 1000);
-          this.authStatusListener.next(true);
+			this.token = authInformation.token;
+			this.isAuthenticated = true;
+			this.setAuthTimer(expiresIn / 1000);
+			this.authStatusListener.next(true);
         }
     }
     
