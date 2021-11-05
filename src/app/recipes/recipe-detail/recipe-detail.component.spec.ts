45dc8d72301534cde120231dd5c8b32e0498e6e9
Michael <michael.kihm@web.de>
2021-11-05 16:03:16 +0100

    Fix wrong indentation

--- src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.spec.ts
@@ -3,23 +3,23 @@ import { RecipeDetailComponent } from './recipe-detail.component';
 
 
 xdescribe('RecipeDetailComponent', () => {
-  let component: RecipeDetailComponent;
-  let fixture: ComponentFixture<RecipeDetailComponent>;
+	let component: RecipeDetailComponent;
+	let fixture: ComponentFixture<RecipeDetailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RecipeDetailComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ RecipeDetailComponent ]
+		})
+		.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RecipeDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(RecipeDetailComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
