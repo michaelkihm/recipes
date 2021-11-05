f8124a791aa6f07239d8d6d9670d6452bf8d35f8
Michael <michael.kihm@web.de>
2021-11-05 11:40:35 +0100

    Add fish to categories

--- models/category.type.ts
+++ models/category.type.ts
@@ -2,6 +2,7 @@ export const ALL_CATEGORIES = [
     'vegan' , 'vegetarian' , 'quick' , 'chicken' , 'pasta' , 'babyFriendly' ,
     'forTheWeekend' , 'salat' , 'italian' , 'spanish' , 'thaiViet' , 'indian' ,
     'healthy' , 'germanMiddleEuropean' , 'bbq' , 'dessert' , 'arabic', 'bowl',
+    'fish',
 ] as const;
 
 export type Category = typeof ALL_CATEGORIES[number];
