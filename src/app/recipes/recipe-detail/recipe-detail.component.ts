e83192d1276963c82df76c250896140cc3075217
Michael <michael.kihm@web.de>
2021-11-25 15:59:56 +0100

    Add bookmark to recipe-detail component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Data, Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Subscription } from 'rxjs';
 import { RecipesService } from '../recipes-service/recipes.service';
 import { Recipe } from './../../../../models/recipe.model';
-import { AuthService } from './../../auth/auth.service';
+import { AuthService, UserInfo } from './../../auth/auth.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.scss']
+	selector: 'app-recipe-detail',
+	templateUrl: './recipe-detail.component.html',
+	styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
 
@@ -16,6 +17,9 @@ export class RecipeDetailComponent implements OnInit {
 	userIsCreator = false;
 	recipe: Recipe;
 	modalRef: BsModalRef;
+	user: UserInfo;
+	isBookmarked = false;
+	userInfoListener: Subscription;
 
   	constructor(
 		  private route: ActivatedRoute,
@@ -25,11 +29,19 @@ export class RecipeDetailComponent implements OnInit {
 		  private modalService: BsModalService) { }
 
 	ngOnInit(): void {
+		
 		this.route.data.subscribe((data: Data) => {
 			this.recipe = data['recipe'];
 		});
 		this.userIsAuthenticated = this.authService.getIsAuth();
 		this.userIsCreator = this.authService.getUserId() === this.recipe.userId;
+		this.user = this.authService.getUser();
+		this.isBookmarked = this.isRecipeBookmarked();
+		
+		this.userInfoListener = this.authService.getUserListener().subscribe(user => {
+			this.user = user;
+			this.isBookmarked = this.isRecipeBookmarked();
+		});
     }
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -46,6 +58,19 @@ export class RecipeDetailComponent implements OnInit {
 				this.router.navigate(['/']);
 			}
 		});
+	}
+
+	private isRecipeBookmarked(): boolean {
+
+		const bookmarks = this.user.bookmarks;
+		if(!bookmarks) return false;
+		return bookmarks.includes(this.recipe.id || '');
+	}
+
+	onBookmark(event: Event): void {
+		event.stopPropagation();
+		const recipeId = this.recipe.id;
+		recipeId && this.authService.upateBookmarks(recipeId, this.isBookmarked ? 'remove' : 'add');
 	}
 
 	isVegan(): boolean {
