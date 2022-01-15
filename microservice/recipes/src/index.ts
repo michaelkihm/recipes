7f34aae41bac2346538b49f4137f009d83e14b46
michaelkihm <michael.kihm@web.de>
2022-01-15 14:55:18 +0100

    Boilerplate Recipes services

---
+++ microservice/recipes/src/index.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+
+  try {
+    await mongoose.connect(
+      `mongodb://${process.env.RECIPES_MONGO_SRV_SERVICE_HOST}:${process.env.RECIPES_MONGO_SRV_SERVICE_PORT}/recipes`,
+      {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true
+    });
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3001, () => {
+    console.log('Listening on port 3001?');
+  });
+};
+
+start();
