1a611b6b9b9e6f90553a9b86e6f125440d4644f2
michaelkihm <michael.kihm@web.de>
2022-01-15 13:46:41 +0100

    use errors and middlewares from package @mickenhosrecipes/common

--- microservice/auth/src/app.ts
+++ microservice/auth/src/app.ts
@@ -2,13 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
+import { NotFoundError, errorHandler } from '@mickenhosrecipes/common';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+
 
 const app = express();
 app.set('trust proxy', true);
@@ -25,7 +24,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async (req, res) => {
+app.all('*', async (_req, _res) => {
   throw new NotFoundError();
 });
 
