b9e35d7b6b416f1c8c9bc13d70c8d0f6127e98f6
michaelkihm <michael.kihm@web.de>
2022-01-25 18:26:52 +0100

    Update createRecipe in __test__/shared

--- microservice/recipes/src/routes/__test__/shared.ts
+++ microservice/recipes/src/routes/__test__/shared.ts
@@ -3,5 +3,16 @@ import request from 'supertest';
 import { app } from '../../app';
 
 export const createRecipe = (recipe: BaseRecipe): request.Test => {
-    return request(app).post('/api/recipes').set('Cookie', global.signin()).send(recipe);
+    
+    return request(app)
+        .post('/api/recipes')
+        .set('Cookie', global.signin())
+        .set('Content-type', 'multipart/form-data')
+        .field('name',recipe.name )
+        .field('userId', recipe.userId)
+        .field('description', JSON.stringify(recipe.description))
+        .field('categories', JSON.stringify(recipe.categories))
+        .field('duration',JSON.stringify(recipe.duration))
+        .field('ingredients',JSON.stringify(recipe.ingredients))
+        .attach('image',recipe.image || '');
 };
