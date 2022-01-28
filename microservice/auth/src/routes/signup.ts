b93aa03b4312c8e51ddbba63f9c8c8e177fecaa4
michaelkihm <michael.kihm@web.de>
2022-01-28 19:13:31 +0100

    Fix indentation

--- microservice/auth/src/routes/signup.ts
+++ microservice/auth/src/routes/signup.ts
@@ -11,52 +11,52 @@ const router = express.Router();
 
 const userSignUp = async (req: UserAddRequest, res: Response<UserDoc>) => {
 
-  //const { email, password } = req.body;
-  const newUser = processImageDataAndFormData(req);
-
-  const existingUser = await UserModel.findOne({ email: newUser.email });
-
-  if (existingUser) throw new BadRequestError('Email in use');
   
-  const user = UserModel.build(newUser);
-  await user.save();
+ 	const newUser = processImageDataAndFormData(req);
 
-  // Generate JWT
-  const userJwt = jwt.sign(
-    {
-      id: user.id,
-      email: user.email
-    },
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    process.env.JWT_KEY!
-  );
+	const existingUser = await UserModel.findOne({ email: newUser.email });
 
-  // Store it on session object
-  req.session = {
-    jwt: userJwt
-  };
+	if (existingUser) throw new BadRequestError('Email in use');
+	
+	const user = UserModel.build(newUser);
+	await user.save();
 
-  res.status(201).send(user);
+	// Generate JWT
+	const userJwt = jwt.sign(
+		{
+		id: user.id,
+		email: user.email
+		},
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		process.env.JWT_KEY!
+	);
+
+	// Store it on session object
+	req.session = {
+		jwt: userJwt
+	};
+
+	res.status(201).send(user);
 };
 
 
 router.post(
-  '/api/users/signup',
-  multerMiddleware('images'),
-  [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters'),
-    body('username')
-      .not().isEmpty()
-      .withMessage('User Model requires property username')
-  ],
-  validateRequest,
-  userSignUp
+  	'/api/users/signup',
+  	multerMiddleware('images'),
+	[
+		body('email')
+		.isEmail()
+		.withMessage('Email must be valid'),
+		body('password')
+		.trim()
+		.isLength({ min: 4, max: 20 })
+		.withMessage('Password must be between 4 and 20 characters'),
+		body('username')
+		.not().isEmpty()
+		.withMessage('User Model requires property username')
+	],
+	validateRequest,
+	userSignUp
 );
 
 export { router as signupRouter };
