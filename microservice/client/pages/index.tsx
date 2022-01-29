fd2245b00a64df973679e24a7ed1b2f7601b2a01
michaelkihm <michael.kihm@web.de>
2022-01-29 19:37:05 +0100

    Get recipes in Home page

--- microservice/client/pages/index.tsx
+++ microservice/client/pages/index.tsx
@@ -1,20 +1,27 @@
 import type { NextPage } from 'next';
+import buildAxiosClient from '../api/build-client';
+import { Recipe } from '@mickenhosrecipes/common';
 
+interface HomeProps {
+	recipes: Recipe[]
+}
 
-const Home: NextPage = () => {
+const Home: NextPage<HomeProps> = ({ recipes }) => {
 	
 	return (
-		<div>
+		<div className='p-2'>
 			<h1>Landing Page</h1>
+			{!recipes.length && <p>No recipes are here</p>}
+			{recipes.map(recipe => <p key={recipe.id}>{recipe.name}</p>)}
 		</div>);
 };
 
 
-// Home.getInitialProps = async (context): Promise<CurrentUser> => {
+Home.getInitialProps = async (context): Promise<HomeProps> => {
 
-// 	//const client = buildAxiosClient(context);
-// 	const { data } = await client.get<CurrentUser>('/api/users/currentuser');
-// 	return data;
-// };
+	const client = buildAxiosClient(context);
+	const { data } = await client.get<Recipe[]>('/api/recipes');
+	return { recipes: data };
+};
 
 export default Home;
