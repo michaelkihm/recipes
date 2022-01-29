31e227d7528b7cb9e05b8226caea1d9cce2db339
michaelkihm <michael.kihm@web.de>
2022-01-29 16:02:25 +0100

    Server side fetch example on Home

--- microservice/client/pages/index.tsx
+++ microservice/client/pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next';
+import buildAxiosClient from '../api/build-client';
+import { UserDoc } from '@mickenhosrecipes/common';
 
-const Home: NextPage = () => {
-  return <h1>Landing Page</h1>;
+type CurrentUser = {
+	currentUser: UserDoc | null;
+};
+
+const Home: NextPage<CurrentUser> = ({ currentUser }) => {
+	
+	return (
+		<div>
+			<h1>Landing Page</h1>
+			{currentUser && <p>Hello {currentUser.username}</p>}
+		</div>);
+};
+
+
+Home.getInitialProps = async (context): Promise<CurrentUser> => {
+
+	const client = buildAxiosClient(context);
+	const { data } = await client.get<CurrentUser>('/api/users/currentuser');
+	return data;
 };
 
 export default Home;
