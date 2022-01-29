6c3de325c1e2119a6e1a49cc3950f183cabea9ee
michaelkihm <michael.kihm@web.de>
2022-01-29 14:07:09 +0100

    Add useRequest hook

---
+++ microservice/client/hooks/use-request.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError } from 'axios';
+import { useState } from 'react';
+import { ErrorResponse, APIErrorResponse } from '@mickenhosrecipes/common';
+import react from 'react'
+
+type ReqeuestProps = {
+    url: string,
+    method: 'post' | 'get' | 'patch' | 'delete',
+    body: {[key: string]: any}
+    onSuccess?: () => void
+}
+
+const useRequest = ({ url, method, body, onSuccess}: ReqeuestProps) => {
+
+    const [errors, setErrors] = useState<ErrorResponse>([]);
+
+    const doRequest = async () => {
+        try {
+            const response = await axios[method](url, body);
+            setErrors([])
+            onSuccess && onSuccess();
+            return response.data
+        } catch (err) {
+            const error = err as AxiosError<APIErrorResponse>;
+            error.response && setErrors(error.response.data.errors);
+        }
+    }
+
+    return { doRequest, errors }
+};
+
+export default useRequest;
