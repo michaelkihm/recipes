27348021817c9f21a7d050a4000862bf0120029f
michaelkihm <michael.kihm@web.de>
2022-02-19 12:49:41 +0100

    Update common package

--- microservice/common/src/events/recipe-events/recipes-deleted-event.ts
+++ microservice/common/src/events/recipe-events/recipes-deleted-event.ts
@@ -1,7 +1,6 @@
 import { Subjects } from '../subjects';
-import { Recipe } from '../../types/recipe.type';
 
 export interface RecipesDeletedEvent {
     subject: Subjects.RecipesDeleted;
-    data: Recipe[]
+    data: string[]
 }
