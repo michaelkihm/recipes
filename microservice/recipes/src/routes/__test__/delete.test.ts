af7d16c8dae7b869d782754beab07eaeb365f7b3
michaelkihm <michael.kihm@web.de>
2022-02-19 12:54:57 +0100

    Publish recipe deleted event

--- microservice/recipes/src/routes/__test__/delete.test.ts
+++ microservice/recipes/src/routes/__test__/delete.test.ts
@@ -1,11 +1,15 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { RecipeDoc, RecipeModel } from '../../models/recipe.model';
+import { natsWrapper } from '../../nats-wrapper';
 import { NEW_RECIPES } from './data/dummy-new-recipes';
 import { createRecipe } from './shared/create-recipe';
 
 
 describe('Delete recipes - /api/recipes/:id', () => {
+
+    beforeEach(() => jest.clearAllMocks());
+
 
     it('has a route handler listening to DELETE /api/recipes/:id', async () => {
 
@@ -52,6 +56,23 @@ describe('Delete recipes - /api/recipes/:id', () => {
         expect(recipes.length).toBe(0);
 
 
+    });
+
+    
+    it('publishes an event if recipe gets deleted', async () => {
+        
+        const newRecipe = NEW_RECIPES[0];
+        const dbRecipe = RecipeModel.build(newRecipe);
+        await dbRecipe.save();
+        const foundRecipes = await RecipeModel.find({ name: newRecipe.name });
+
+        await request(app)
+          .delete(`/api/recipes/${foundRecipes[0]._id}`)
+          .set('Cookie', global.signin())
+          .send({})
+          .expect(200);
+
+        expect(natsWrapper.client.publish).toHaveBeenCalledTimes(1);
     });
 
 });
--- microservice/recipes/src/routes/delete.ts
+++ microservice/recipes/src/routes/delete.ts
@@ -1,6 +1,8 @@
 import { BadRequestError, requireAuth } from '@mickenhosrecipes/common';
 import express, { Request, Response } from 'express';
+import { RecipeDeletedPublisher } from '../events/publishers/recipe-deleted-publisher';
 import { RecipeModel } from '../models/recipe.model';
+import { natsWrapper } from '../nats-wrapper';
 
 type DeleteRecipeResponse = {
     message: string;
@@ -13,6 +15,7 @@ const deleteRecipe = async (req: Request<{id: string}>, res: Response<DeleteReci
     try {
         await RecipeModel.findByIdAndDelete(req.params.id);
         res.status(200).send({ message: `Deleted recipe ${req.params.id}` });
+        await new RecipeDeletedPublisher(natsWrapper.client).publish([req.params.id]);
     } catch (err) {
         throw new BadRequestError(`Recipe with id ${req.params.id} does not exist and can not be deleted`);
     }
