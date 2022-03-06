366a19432721df65032011ffd50de21368f9d6d5
michaelkihm <michael.kihm@web.de>
2022-03-06 21:41:01 +0100

    Style Recipe Details page

--- microservice/client/pages/recipe/[id].tsx
+++ microservice/client/pages/recipe/[id].tsx
@@ -1,8 +1,8 @@
 import { GetServerSideProps, NextPage } from 'next';
 import { Recipe } from '@mickenhosrecipes/common';
 import { buildAxiosBackendClient } from '../../api/server-side-axios-client';
-import Image from 'next/image';
-import imageLoader from '../../api/image-loader';
+import RecipeThematicBreak from '../../components/RecipeThematicBreak';
+import BulletPointIcon from '../../components/icons/BulletPointIcon';
 
 interface RecipePageProps {
     recipe: Recipe
@@ -10,17 +10,46 @@ interface RecipePageProps {
 
 const RecipePage: NextPage<RecipePageProps> = ({ recipe }) => {
 
-    const { name, image } = recipe;
+    const { name, image, duration, categories, ingredients, description } = recipe;
 
     return (
-        <div>
-            <h1>Hallo Rezept {name}</h1>
-            {image && <Image src={image} alt={name} width={200} height={200} loader={imageLoader} unoptimized/>}
+        <div className="h-full flex flex-col gap-y-2 overflow-y-auto scroll-container">
+            <h1 className="text-3xl font-mono">{name}</h1>
+            <div className="flex gap-x-1">
+                <p className="font-bold">Zeit:</p>
+                <p>{duration.duration}</p>
+                <p>{duration.unit}</p>
+            </div>
+            {/* {image && <Image src={image} alt={name} width={200} height={200} loader={imageLoader} unoptimized/>} */}
+            { image && <img src={image} alt={name} height={'25%'}/>}
+            { categories.map(category => (
+                <div key={category} className="max-w-[25%] p-0.5 flex justify-center items-center rounded-lg bg-chenin">
+                    <p className='text-black'>{category}</p>
+                </div>
+            ))}
+            <RecipeThematicBreak name="Zutaten" />
+            <ul className="list-inside">
+                {ingredients.map(ingredient => (
+                    <li key={`${ingredient.name}:${ingredient.amount}`} className="flex items-center gap-x-1">
+                        <BulletPointIcon />
+                        <p>{ingredient.amount}</p>
+                        <p>{ingredient.unit}</p>
+                        <p>{ingredient.name}</p>
+                    </li>
+                ))}
+            </ul>
+
+            <RecipeThematicBreak name="Zubereitung" />
+            {description.map((step,i) => (
+                <div key={i}>
+                    <p className="font-bold">Schritt {i + 1}</p>
+                    <p>{step}</p>
+                </div>
+            ))}
         </div>
     );
 };
 
-export default RecipePage;
 
 export const getServerSideProps: GetServerSideProps<RecipePageProps> = async (context) => {
     
@@ -33,3 +62,5 @@ export const getServerSideProps: GetServerSideProps<RecipePageProps> = async (co
         },
     };
 };
+
+export default RecipePage;
