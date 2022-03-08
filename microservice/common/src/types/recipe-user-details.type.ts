72e3a9ffb34b18b1373a851a2554500c5af98324
michaelkihm <michael.kihm@web.de>
2022-03-08 17:15:52 +0100

    Update common package

--- microservice/common/src/types/recipe-user-details.type.ts
+++ microservice/common/src/types/recipe-user-details.type.ts
@@ -5,3 +5,10 @@ export interface RecipeUserDetails extends Omit<BaseRecipe, 'userId'> {
     userId: UserEvent,
     id: string
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isRecipeUserDetails = (obj: RecipeUserDetails['userId'] | any) : obj is RecipeUserDetails['userId'] => {
+    return (obj && obj.username && typeof obj.username === 'string' &&
+        obj.email && typeof obj.email === 'string' &&
+        obj.image && typeof obj.image === 'string');
+};
