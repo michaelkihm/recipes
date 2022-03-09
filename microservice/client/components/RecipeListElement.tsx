c47ea1882a5aea26f70efbfe9938e865db74e9f9
michaelkihm <michael.kihm@web.de>
2022-03-09 16:09:46 +0100

    Link to RecipeListElement

--- microservice/client/components/RecipeListElement.tsx
+++ microservice/client/components/RecipeListElement.tsx
@@ -1,18 +1,20 @@
 import { FunctionComponent } from 'react';
 import { Recipe } from '@mickenhosrecipes/common';
+import Link from 'next/link';
 
 const RecipeListElement: FunctionComponent<{recipe: Recipe}> = ({ recipe }) => {
 
-    const { name, image } = recipe;
+    const { name, image, id } = recipe;
 
     return (
-        <div className="w-11/12 p-1 flex items-center gap-x-6 drop-shadow-lg border border-black">
-            {image && <div className="w-1/4">
-                <img src={image} alt={name} className="h-12"/>
-            </div>}
-            <p className="self-start font-bold">{name}</p>
-        </div>
-    );
+        <Link href={`/recipe/${id}`} passHref>
+            <div className="w-11/12 p-1 flex items-center gap-x-6 drop-shadow-lg border border-black">
+                {image && <div className="w-1/4">
+                    <img src={image} alt={name} className="h-12"/>
+                </div>}
+                <p className="self-start font-bold">{name}</p>
+            </div>
+        </Link>);
 };
 
 export default RecipeListElement;
