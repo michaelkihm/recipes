c1926dcd0a78c89cc7df1a08eb3c7393f9fb9abd
michaelkihm <michael.kihm@web.de>
2022-03-09 15:58:33 +0100

    Userpage fetch and display bookmarks and user recipes

--- microservice/client/pages/user/index.tsx
+++ microservice/client/pages/user/index.tsx
@@ -1,13 +1,23 @@
+import { Recipe } from '@mickenhosrecipes/common';
+import { AxiosResponse } from 'axios';
+import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import { FunctionComponent, useContext } from 'react';
 import imageLoader from '../../api/image-loader';
+import { buildAxiosBackendClient } from '../../api/server-side-axios-client';
+import RecipeListElement from '../../components/RecipeListElement';
+import RecipeThematicBreak from '../../components/RecipeThematicBreak';
 import UserContext from '../../context/user-context';
 
-const UserPage: FunctionComponent = ({}) => {
+interface UserPageProps {
+    booksmarks: Recipe[];
+    recipes: Recipe[];
+}
+const UserPage: FunctionComponent<UserPageProps> = ({ booksmarks, recipes }) => {
 
     const { currentUser } = useContext(UserContext);
 
-    if(!currentUser) return <p>Kein Benutzer eingeloggt</p>;
+    if(!currentUser) return <p className="p-2 bg-danger">Kein Benutzer eingeloggt</p>;
 
     return (
         <div className='flex flex-col '>
@@ -20,10 +30,39 @@ const UserPage: FunctionComponent = ({}) => {
                         loader={imageLoader} unoptimized/>
                 </div>}
             <p>{currentUser.username}</p>
-            <h2 className="text-2xl text-bold font-mono">Rezepteliste</h2>
-            <h2 className="text-2xl text-bold font-mono">Meine Rezepte</h2>
+
+            <RecipeThematicBreak name="Rezepteliste"/>
+            <div className="my-2 flex flex-col gap-y-2">
+                {!booksmarks.length && <p>Keine Rezepte hier...</p>}
+                {booksmarks.map(recipe => <RecipeListElement recipe={recipe} key={recipe.id}/>)}
+            </div>
+            
+            <RecipeThematicBreak name="Meine Rezepte"/>
+            <div className="my-2 flex flex-col gap-y-2">
+                {!recipes.length && <p>Keine Rezepte hier...</p>}
+                {recipes.map(recipe =><RecipeListElement recipe={recipe} key={recipe.id}/>)}
+            </div>
         </div>
     );
+};
+
+export const getServerSideProps: GetServerSideProps<UserPageProps> = async (context) => {
+
+    const client = buildAxiosBackendClient(context.req.headers);
+    let booksmarksResponse: AxiosResponse<Recipe[], any> | undefined;
+    let recipesResponse: AxiosResponse<Recipe[], any> | undefined;
+    
+    if(Object.keys(context.req.cookies).length) {
+        booksmarksResponse = await client.get<Recipe[]>('/api/recipes/bookmarks');
+        recipesResponse = await client.get<Recipe[]>('/api/recipes?userId=currentUser');
+    }
+    
+    return {
+        props: {
+            booksmarks: booksmarksResponse ? booksmarksResponse.data : [],
+            recipes: recipesResponse ? recipesResponse.data : [],
+        },
+    };
 };
 
 export default UserPage;
