5d9d6a0b9e4907357ebb1255c083288c815264f5
michaelkihm <michael.kihm@web.de>
2022-03-10 11:39:48 +0100

    Custom Button component

---
+++ microservice/client/components/Button.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react';
+
+type BtnStyles = 'danger' | 'black' | 'white';
+// eslint-disable-next-line max-len
+export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes {
+    color: BtnStyles;
+}
+
+const btnStyles: {[key in BtnStyles]: string} = {
+    danger: 'bg-danger text-white',
+    black: 'bg-stone-800 text-white',
+    white: 'bg-white text-black border border-black'
+};
+
+
+const Button: FunctionComponent<ButtonProps> = (props) => {
+
+    const { children, color, className, ...rest } = props;
+
+    return (
+        <button {...rest}
+            className={`h-7 p-1 flex items-center justify-center rounded shadow ${btnStyles[color]} ${className}`}>
+                {children}
+        </button>);
+};
+
+export default Button;
