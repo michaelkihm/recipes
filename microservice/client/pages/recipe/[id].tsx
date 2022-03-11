79c45db85b814f09cd49bdaa7d1db487ff2afb85
michaelkihm <michael.kihm@web.de>
2022-03-11 14:35:50 +0100

    Delete Recipe functionality

--- microservice/client/pages/recipe/[id].tsx
+++ microservice/client/pages/recipe/[id].tsx
@@ -5,71 +5,107 @@ import RecipeThematicBreak from '../../components/RecipeThematicBreak';
 import BulletPointIcon from '../../components/icons/BulletPointIcon';
 import RecipeImage from '../../components/RecipeImage';
 import { AxiosResponse } from 'axios';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, Fragment } from 'react';
 import UserContext from '../../context/user-context';
 import { updateBookmarksRequest } from '../../api/update-bookmarks';
 import BookmarkIcon from '../../components/icons/BookmarkIcon';
+import CloseIcon from '../../components/icons/CloseIcon';
+import Modal from '../../components/Modal';
+import Button from '../../components/Button';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+import ErrorDialog from '../../components/ErrorDialog';
 
 interface RecipePageProps {
     recipe: RecipeUserDetails;
     bookmarks: string[];
 }
 
+const ICON_SIZE_REM = 2;
+
 const RecipePage: NextPage<RecipePageProps> = ({ recipe, bookmarks }) => {
 
     const { name, image, duration, categories, ingredients, description, userId, id } = recipe;
     const [bookmarked, setBookmarked] = useState<boolean>(bookmarks.includes(id));
+    const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
 
     const { currentUser } = useContext(UserContext);
+    const { doRequest, errors } = useRequest({
+        url: `/api/recipes/${id}`,
+        method: 'delete',
+        body: {},
+        onSuccess: () => Router.push('/') 
+    })
+
+    const isAuthor = () => currentUser?.email === userId.email;
+
+    const deleteRecipeHandler = () => {
+        setShowDeleteModal(false);
+        doRequest();
+    }
 
     useEffect(() => {
         currentUser && updateBookmarksRequest(bookmarked ? 'push' : 'pull', id);
     },[bookmarked])
 
     return (
-        <div className="h-full flex flex-col gap-y-2 overflow-y-auto scroll-container">
-            <div className="w-full mb-2 flex justify-between">
-                <div className="grow relative">
-                    <h1 className="text-3xl font-mono">{name}</h1>
-                    <p className="absolute top-6 text-gray-400 font-bold">by {userId.username}</p>
+        <Fragment>
+            <div className="h-full flex flex-col gap-y-2 overflow-y-auto scroll-container">
+                <div className="w-full mb-2 flex justify-between">
+                    <div className="grow relative">
+                        <h1 className="text-3xl font-mono">{name}</h1>
+                        <p className="absolute top-6 text-gray-400 font-bold">by {userId.username}</p>
+                    </div>
+                    <div className="flex gap-x-1">
+                        {isAuthor() && <CloseIcon sizeRem={ICON_SIZE_REM} className="bg-danger rounded" onClick={() => setShowDeleteModal(true)}/>}
+                        {currentUser && <BookmarkIcon checked={bookmarked} sizeRem={ICON_SIZE_REM} onClick={() => setBookmarked(!bookmarked)}/>}
+                    </div>
                 </div>
-                {currentUser && <BookmarkIcon checked={bookmarked} sizeRem={2} onClick={() => setBookmarked(!bookmarked)}/>}
-            </div>
-            <div className="flex gap-x-1">
-                <p className="font-bold">Zeit:</p>
-                <p>{duration.duration}</p>
-                <p>{duration.unit}</p>
-            </div>
-            {/* {image && <Image src={image} alt={name} width={200} height={200} loader={imageLoader} unoptimized/>} */}
-            { image && <RecipeImage imagePath={image} name={name} categories={categories} className="h-[75%]"/>}
-            <div className="flex flex-wrap gap-x-1">
-                { categories.map(category => (
-                    <div key={category}
-                        className="min-w-[25%] px-1 py-0.5 flex justify-center items-center rounded-lg bg-chenin">
-                            <p className='text-black'>{category}</p>
+                {errors.length && <ErrorDialog errors={errors}/>}
+                <div className="flex gap-x-1">
+                    <p className="font-bold">Zeit:</p>
+                    <p>{duration.duration}</p>
+                    <p>{duration.unit}</p>
+                </div>
+                {/* {image && <Image src={image} alt={name} width={200} height={200} loader={imageLoader} unoptimized/>} */}
+                { image && <RecipeImage imagePath={image} name={name} categories={categories} className="h-[75%]"/>}
+                <div className="flex flex-wrap gap-x-1">
+                    { categories.map(category => (
+                        <div key={category}
+                            className="min-w-[25%] px-1 py-0.5 flex justify-center items-center rounded-lg bg-chenin">
+                                <p className='text-black'>{category}</p>
+                        </div>
+                    ))}
+                </div>
+                <RecipeThematicBreak name="Zutaten" />
+                <ul className="list-inside">
+                    {ingredients.map(ingredient => (
+                        <li key={`${ingredient.name}:${ingredient.amount}`} className="flex items-center gap-x-1">
+                            <BulletPointIcon />
+                            <p>{ingredient.amount}</p>
+                            <p>{ingredient.unit}</p>
+                            <p>{ingredient.name}</p>
+                        </li>
+                    ))}
+                </ul>
+                <RecipeThematicBreak name="Zubereitung" />
+                {description.map((step,i) => (
+                    <div key={i}>
+                        <p className="font-bold">Schritt {i + 1}</p>
+                        <p>{step}</p>
                     </div>
                 ))}
             </div>
-            <RecipeThematicBreak name="Zutaten" />
-            <ul className="list-inside">
-                {ingredients.map(ingredient => (
-                    <li key={`${ingredient.name}:${ingredient.amount}`} className="flex items-center gap-x-1">
-                        <BulletPointIcon />
-                        <p>{ingredient.amount}</p>
-                        <p>{ingredient.unit}</p>
-                        <p>{ingredient.name}</p>
-                    </li>
-                ))}
-            </ul>
-
-            <RecipeThematicBreak name="Zubereitung" />
-            {description.map((step,i) => (
-                <div key={i}>
-                    <p className="font-bold">Schritt {i + 1}</p>
-                    <p>{step}</p>
+            {showDeleteModal && <Modal onClose={() => setShowDeleteModal(false)} title="Rezept löschen">
+                <div className="p-1 flex flex-col gap-y-2 overflow-x-hidden">
+                    <p>Möchtest du das Rezept {name} löschen?</p>
+                    <div className="pt-2 px-1 pb-1 flex justify-end gap-x-1">
+                        <Button color="black" onClick={deleteRecipeHandler}>OK</Button>
+                        <Button color="white" onClick={() => setShowDeleteModal(false)}>Abbrechen</Button>
+                    </div>
                 </div>
-            ))}
-        </div>
+                </Modal>}
+        </Fragment>
     );
 };
 
